4821b6b4bd8f4e3eccaffc290407c0d5630c3ea1
yash sharma <yashsharma373@gmail.com>
2021-06-10 23:57:29 +0530

    Add hash to passwords

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -25,10 +25,6 @@ const userSchema = new mongoose.Schema({
     password : String
 });
 
-
-userSchema.plugin(encrypt, {secret : process.env.SECRET , encryptedFields : ["password"]});
-userSchema.plugin(encrypt.encryptedChildren);
-
 const User = new mongoose.model("User",userSchema);
 
 app.get('/',(req,res)=>{
@@ -46,7 +42,7 @@ app.get('/register',(req,res)=>{
 app.post('/register',(req,res)=>{
     const newUser = new User({
         email : req.body.username,
-        password : req.body.password
+        password : md5(req.body.password)
     })
 
     newUser.save((err)=>{
@@ -61,7 +57,7 @@ app.post('/register',(req,res)=>{
 
 app.post('/login',(req,res)=>{
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     
 
